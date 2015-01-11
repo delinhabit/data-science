60cc06c1417e264ca14375aaf89f581ee70a8f48
Ion Scerbatiuc <ion@rover.com>
2015-01-11 09:53:12 -0800

    [getdata-week-1] Added utilities for week_1 of getting data course quiz

---
+++ getting_data/week_1/utils.R
@@ -0,0 +1,19 @@
+downloadData <- function(url, filename) {
+    # Download the data from the provided URL and return the path to the
+    # downloaded file.
+    #
+    # Arguments
+    #    url - the URL from wich to download the data file
+    #    filename - the name of the destination file
+    #
+    if (!file.exists("data")) {
+        dir.create("data")
+    }
+
+    destfile = paste(c("data", filename), collapse="/")
+    if (!file.exists(destfile)) {
+        download.file(url, destfile = destfile, method = "curl")
+    }
+
+    destfile
+}
