5b9efd4231cb7917e44af82f60cebb6cfc8018a2
Ion Scerbatiuc <ion@rover.com>
2015-02-21 08:12:35 -0800

    [exdata-project-2] Implemented the code for plot1.png

--- expdata-analysis/plot1.R
+++ expdata-analysis/plot1.R
@@ -18,9 +18,26 @@ if (!all(file.exists(nei_file, scc_file))) {
 
 ### Load the data
 
-nei_data <- readRDS(nei_file)
-scc_data <- readRDS(scc_file)
+#nei_data <- readRDS(nei_file)
+#scc_data <- readRDS(scc_file)
 
 ### Analyze and plot
 
+emissions_by_year <- nei_data %>%
+    group_by(year) %>%
+    summarise(total = sum(Emissions)) %>%
+    mutate(total = total / 1e6)
 
+png("plot1.png")
+
+plot(
+    emissions_by_year,
+    pch = 19,
+    xlab = "Year",
+    ylab = "Emissions (millions of tons)")
+
+model <- lm(total ~ year, emissions_by_year)
+abline(model, lwd = 1, col = "darkblue")
+
+# Use invisible() to silence errors when running with Rscript
+invisible(dev.off())
