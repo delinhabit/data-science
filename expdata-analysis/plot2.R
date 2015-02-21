040f28f1481c8fab5928252fc65c41355cabee31
Ion Scerbatiuc <ion@rover.com>
2015-02-21 09:36:00 -0800

    [exdata-project-2] Implemented code for plot2.png

---
+++ expdata-analysis/plot2.R
@@ -0,0 +1,42 @@
+library(dplyr)
+
+### Download the data file if not already downloaded
+
+if (!file.exists("data")) {
+    dir.create("data")
+}
+
+nei_file = "data/summarySCC_PM25.rds"
+
+if (!file.exists(nei_file)) {
+    url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    data_file = "data/NEI_data.zip"
+    download.file(url, destfile = data_file, method = "curl")
+    unzip(data_file, exdir = "data/")
+}
+
+### Load the data
+
+nei_data <- readRDS(nei_file)
+
+### Analyze and plot
+
+baltimore_emissions_by_year <- nei_data %>%
+    filter(fips == "24510") %>%
+    group_by(year) %>%
+    summarise(total = sum(Emissions)) %>%
+    mutate(total = total / 1e3)
+
+png("plot2.png")
+
+plot(
+    baltimore_emissions_by_year,
+    pch = 19,
+    xlab = "Year",
+    ylab = "Emissions (thousands of tons)")
+
+model <- lm(total ~ year, baltimore_emissions_by_year)
+abline(model, lwd = 1, col = "darkblue")
+
+# Use invisible() to silence errors when running with Rscript
+invisible(dev.off())
